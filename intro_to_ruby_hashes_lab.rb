31a0d159cc318d2288120c113a6a6614233c44f0
Aaron Minyard <amnyrrd@gmail.com>
2020-01-03 23:06:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,9 +10,6 @@ def monopoly_with_second_tier
 	monopoly = {
 	  :railroads=> {
 	    :pieces=> 4,
-	    :rent_in_dollars=> {
-	      
-	    }
 	    }
 	  }
 end
@@ -21,6 +18,9 @@ def monopoly_with_third_tier
 	monopoly = {
 	  :railroads=> {
 	    :pieces=> 4
+	    :rent_in_dollars=> {
+	      
+	    }
 	    }
 	  }
 end
